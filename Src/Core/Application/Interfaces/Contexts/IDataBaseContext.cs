d7784f08c10d4d5d8bab8e25bf3d16a39a3d7567
Milad Heidarpour <mld.heidarpour@gmail.com>
2025-02-10 11:53:50 +0330

    Add Sql DataBase

---
+++ Src/Core/Application/Interfaces/Contexts/IDataBaseContext.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Users;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Interfaces.Contexts;
+
+public interface IDataBaseContext
+{
+    public DbSet<User> Users { get; set; }
+
+    int SaveChanges();
+    int SaveChanges(bool acceptAllChangesOnSuccess);
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken());
+    Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = new CancellationToken());
+}
---
+++ Src/Infrastructures/Persistence/Contexts/DatabaseContext.cs
@@ -0,0 +1,14 @@
+﻿using Application.Interfaces.Contexts;
+using Domain.Users;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence.Contexts;
+
+public class DatabaseContext:DbContext,IDataBaseContext
+{
+    public DatabaseContext(DbContextOptions options):base(options)
+    {
+        
+    }
+    public DbSet<User> Users { get; set; }
+}
