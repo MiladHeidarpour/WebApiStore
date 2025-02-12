68040ff31cddef83b202b5b1c128a1f2b12d47f7
Milad Heidarpour <mld.heidarpour@gmail.com>
2025-02-12 16:21:03 +0330

    Create RegisterUser Task

--- Src/Presentation/WebSite.EndPoint/Controllers/AccountController.cs
+++ Src/Presentation/WebSite.EndPoint/Controllers/AccountController.cs
@@ -1,12 +1,60 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Domain.Users;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebSite.EndPoint.Models.ViewModels.Register;
 
-namespace WebSite.EndPoint.Controllers
+namespace WebSite.EndPoint.Controllers;
+
+public class AccountController : Controller
 {
-    public class AccountController : Controller
+    private readonly UserManager<User> _userManager;
+
+    public AccountController(UserManager<User> userManager)
     {
-        public IActionResult Login()
+        _userManager = userManager;
+    }
+
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Register(RegisterViewModel model)
+    {
+        if (!ModelState.IsValid)
         {
             return View();
         }
+
+        User newUser = new User()
+        {
+            Email = model.Email,
+            UserName = model.Email,
+            FullName = model.FullName,
+            PhoneNumber = model.PhoneNumber,
+        };
+
+        var result = _userManager.CreateAsync(newUser, model.Password).Result;
+        if (result.Succeeded)
+        {
+            return RedirectToAction(nameof(Profile));
+        }
+
+        foreach (var item in result.Errors)
+        {
+            ModelState.AddModelError(item.Code, item.Description);
+        }
+        return View(model);
+    }
+
+    public IActionResult Profile()
+    {
+        return View();
     }
 }
